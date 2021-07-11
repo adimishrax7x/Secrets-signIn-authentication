27dd2813ce1f2442454ab895e6a26ddf69a69f55
Aditya Mishra <adimish777@gmail.com>
2021-07-11 16:48:19 +0530

    Hashing encrption

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-var encrypt = require('mongoose-encryption');
+// var encrypt = require('mongoose-encryption');
+var md5 = require('md5');
 
 
 port=3000 || process.env.PORT ;
@@ -23,7 +24,7 @@ const userSchema= new mongoose.Schema({
   'password':String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']  });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']  });
 
 const User=new mongoose.model("User",userSchema);
 
@@ -39,7 +40,7 @@ app.route('/login')
 })
   .post(function(req, res){
     let loginEmail= req.body.username;
-    let loginPassword= req.body.password;
+    let loginPassword= md5(req.body.password);
 
     User.findOne({email:loginEmail},function(err,foundItem){
       if(!err){
@@ -63,7 +64,7 @@ app.route('/register')
   .post(function(req, res){
     const newUser=new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     newUser.save(function(err){
       if(err){
